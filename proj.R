c1e16a9cd78e0bdd94152ca550376a84310bf859
Ed Auer <edauer1@gmail.com>
2019-08-28 13:34:54 -0400

    Update of project start with hash table and lookup

--- proj.R
+++ proj.R
@@ -1,5 +1,3 @@
-
-
 library(tidyverse)
 library(tidytext)
 
@@ -13,3 +11,35 @@ temp2<- unnest_tokens(temp, word, X3)
 foo<-group_by(temp2, X2) %>% 
   +     summarize(text = str_c(word, collapse = " ")) %>%
   +     ungroup()
+
+
+subtl <- new.env(hash = TRUE, parent = emptyenv())
+
+# read in a paired list file and create a hash for lookup
+
+tempf <-read_csv('SUBTLus.csv',col_names = FALSE, col_types = cols("c","n"))
+ltempf = nrow(tempf)
+for(i in 1:ltempf)
+{
+  subtl[[ tolower(tempf$X1[i]) ]]<- tempf$X2[i]
+}
+rm(tempf)
+
+# end read in a paired list file and create a hash for lookup
+
+templex <-read_csv('.csv',col_names = FALSE, col_types = cols("c","n"))
+
+
+
+f <- function(x){
+  if(exists_subtl(tolower(x[3]),subtl)){
+    get_subtl(tolower(x[3]),subtl)
+  }
+  else{return("NA")}
+}
+
+exists_subtl <- Vectorize(exists, vectorize.args = "x")
+
+
+
+apply(temp2,1,f)
